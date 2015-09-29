33049bbe4cc01cfe607221be31d7f014f56519b2
palcas <palcas69@gmail.com>
2015-09-29 14:07:14 +0300

    Originalas

---
+++ page-16.php
@@ -0,0 +1,98 @@
+﻿<?php get_header(); ?>
+
+
+<div id="page_content" class="row">
+	<div class="col-sm-12">
+		<p class="text-right no_margin the_title heading_line">
+			informacija
+			
+		</p>
+		<div class="content_box">
+			<p class="text-right sub_title">
+				<?php the_title();?>.
+			</p>
+<br/><br/>
+<p class="sub_content text-center">
+<a href="http://neformalusugdymas.kaunas.lt/wp-content/uploads/sites/11/2015/09/Dėl-programų-tvirtinimo.doc" target="_blank">
+Patvirtintos neformaliojo vaikų švietimo programos.
+</a>
+</p>	
+<p class="sub_content text-center">_________</p>	
+<p class="sub_content text-center">
+<a href="http://neformalusugdymas.kaunas.lt/wp-content/uploads/sites/11/2015/09/isakymas_neformalus_ugdymas.pdf" target="_blank">
+Kauno miesto savivaldybės švietimo teikėjų, atitinkančių reikalavimus įgyvendinti neformaliojo vaikų švietimo programas, sąrašas.
+</a>
+</p>	
+<p class="sub_content text-center">_________</p>		
+<p class="sub_content text-center">
+Pretenduojančius gauti krepšelio lėšas neformaliojo vaikų švietimo programų vykdymui, kviečiame neformaliojo vaikų švietimo paslaugų teikėjus <span class="orange">š.m. rugsėjo 9-14 dienomis 8-17 val. (Pietų pertrauka 12-12,45 val.) </span>pateikti  Kauno miesto savivaldybės  administracijos Švietimo ir ugdymo skyriui (M.Dobužinskio g. 6) dokumentus:
+</p>
+<ul class="sub_content_ul text-center">
+<li><strong><span class="orange"><u>NVŠ teikėjo atitikties reikalavimams nustatymo paraišką</u></span></strong> (Neformaliojo vaikų švietimo teikėjų atitikties reikalavimams nustatymo, programų vertinimo ir atsiskaitymo už gautas lėšas tvarkos aprašo, patvirtinto Kauno miesto savivaldybės administracijos direktoriaus 2015 m. rugsėjo 9 d. įsakymu Nr. A-25932 priedas Nr.2) Du šios paraiškos egzemplioriai, jos el. variantas WORD formatu ir kiti šioje paraiškoje numatyti dokumentai pateikiami vyr. spec.  A.Panfilaitei, 18 kab.;</li>
+<li><strong><span class="orange"><u>NVŠ programos atitikties reikalavimams nustatymo paraišką </u></span></strong> (pirmame punkte nurodyto dokumento 4 priedas). Du šios paraiškos egzemplioriai, jos el. variantas WORD formatu pateikiami vyr. spec. J.Česnulevičienei, 3 kab.</li>
+</ul>
+<p class="sub_content text-center">
+<strong><u>Pastaba:</u></strong> Prieš pateikiant dokumentus Švietimo ir ugdymo skyriui jie turi būti užregistruoti ir prisegti:
+</p>
+<ul class="sub_content_ul text-center">
+<li>Juridiniams asmenims – įstatus, nuostatus – ŠMIR;</li>
+<li>Programos atitikties paraiška – KTPRR;</li>
+</ul>
+<p class="sub_content text-center">Į Programos atitikties paraišką <span class="orange">nepamiršti įrašyti KTPRR suteikto kodo</span>, atspausdinti ir pasirašyti.</p>
+<p class="sub_content text-right">Švietimo ir ugdymo skyrius</p>
+<p class="sub_content text-center">_________</p>
+
+<p class="sub_content text-center"><span class="orange">Rugsėjo 11 d.</span> (penktadienį) <span class="orange">13.00 val.</span> neformaliojo vaikų švietimo teikėjus (nevyriausybinių organizacijų, biudžetinių įstaigų atstovus, laisvuosius mokytojus) kviečiame dalyvauti tiesioginėje vaizdo transliacijoje internetu apie neformaliojo vaikų švietimo lėšų (toliau – NVŠ) skyrimą ir naudojimą</p>
+<p class="sub_content text-center">Transliacijos metu bus aptariami reikalavimai, keliami NVŠ teikėjams ir jų rengiamoms programoms, atsakoma į kylančius aktualius klausimus</p>
+<p class="sub_content text-center">Tiesioginės vaizdo transliacijos metu bus galima tiesiogiai užduoti klausimus, pateikti nuomonę. Klausimus taip pat galima iš anksto siųsti Giedrei Tumosaitei, el.p. <a href="mailto:giedre.tumosaite@upc.smm.lt" class="orange">giedre.tumosaite@upc.smm.lt</a>, tel. (8 5) 279 9599.</p>
+<p class="sub_content text-center">Kviečiame jungtis prie tiesioginės transliacijos Mokytojo TV  ir aktyviai dalyvauti, užduoti klausimus Ugdymo plėtotės centro Neformaliojo ugdymo skyriaus darbuotojams.</p>
+<p class="sub_content text-center">Jungtis prie tiesioginės transliacijos galima nuo 12.55 val.</p>
+<p class="sub_content text-center">Prie UPC vaizdo transliacijų svetainės prisijungsite vadovaudamiesi <a href="https://docs.google.com/file/d/0B1vajc81YparTFFleURDYnhWZDA/edit?pli=1" target="_blank" class="orange">prisijungimo instrukcija</a></p>
+
+
+<p class="sub_content text-center">_________</p>
+
+
+<p class="sub_content text-center">
+Visi neformaliojo vaikų švietimo paslaugų teikėjai, ketinantys pretenduoti į valstybės skiriamas krepšelio lėšas iki rugsėjo mėnesio 8 d.  turi užsiregistruoti Švietimo ir mokslo institucijų registre (toliau - ŠMIR), o jų programos užregistruotos Kvalifikacijos  tobulinimo programų ir renginių registre (toliau -  KTPRR).
+</p>
+<p class="sub_content text-center">_________</p>
+<p class="sub_content text-center">
+Prisiregistruoti ŠMIR galima:
+</p>
+<p class="sub_content text-center">
+1. Kreiptis į savivaldybės ŠMIR tvarkytoją Jolantą Ganusauskienę (tel. (8 27) 425401 arba el.p. jolanta.ganusauskiene@kaunas.lt) ir užpildžius duomenų registravimo kortelę pateikti specialistei  kartu su   nuostatais/įstatais. 
+</p>
+<p class="sub_content text-center">
+2. Pačiam paslaugų teikėjui prisijungus prie  ŠMIR adresu https://www.smir.smm.lt spausti „tapti naudotoju“. Užsiregistravus suvesti visus savo įstaigos duomenis.
+</p>
+<p class="sub_content text-center">
+3. Pačiam paslaugų teikėjui prisijungus prie KTPRR adresu https://www.ktprr.smm.lt,  spausti „tapti naudotoju“. užpildyti  pasižadėjimą bei teikėjo anketą ir išsiusti ją į ITC. ITC darbuotojas Mažvydas Jablonskis (el.p. mazvydas.jablonskis@itc.smm.lt) suteikia paslaugų teikėjui prisijungimo kodą. Gavus prisijungimo teisę, suvesti reikalingą informaciją apie programas į KTPRR.
+</p>
+<p class="sub_content text-center">_________</p>
+<p class="sub_content text-center">
+Iškilus klausimams galima kreiptis į savivaldybės švietimo duomenų tvarkytoją Jolantą Ganusauskienę arba į ŠMM Informacinių technologijų centro  (toliau – ITC) darbuotoją Egidijų Čeponį (el.p.: Egidijus.ceponis@itc.smm.lt)
+</p>
+<p class="sub_content text-center">_________</p>
+<p class="sub_content text-center">
+NVŠ teikėjams informacija teikiama:
+</p>
+
+<ul class="sub_content_ul text-center">
+<li>Prisijungimo prie duomenų bazių klausimais: Jolanta Ganusauskienė, tel. (8 27) 425401 arba el.p. jolanta.ganusauskiene@kaunas.lt);</li>
+<li>Programų ir kitų dokumentų rengimo klausimais: Vida Kučiauskienė, tel. (8 27) 423766 arba el. p. vida.kuciauskiene@kaunas.lt; Alvyda Panfilaitė, tel. (8 27) 425479 arba el.p. alvyda.panfilaite@kaunas.lt;</li>
+<li>Sportinės krypties programų ir kitų dokumentų rengimo klausimais: Jovilė Bartnikienė, tel. (8 27) 73 34 49 arba el.p. jovile.bartnikiene@kaunas.lt</li>
+</ul>
+
+		</div>		
+
+		<p class="text-center back-btn-wrap"><a class="back_btn" href="#">atgal</a></p>
+	</div>
+	
+
+</div>
+	
+
+
+
+<?php get_footer(); ?>
